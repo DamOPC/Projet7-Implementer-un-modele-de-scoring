a5d647864c0cc609789e868df92c94cbd4422a8d
DamOPC <90404008+DamOPC@users.noreply.github.com>
2022-04-13 11:03:05 +0200

    Update app.py

--- app.py
+++ app.py
@@ -74,7 +74,8 @@ def shap():
     df_test = df[df['sk_id_curr']==100010]
     y_pred = estimator.predict_proba(df_test)
     zero_proba = y_pred[0,0]
-    return json.dumps({'pred' : zero_proba})
+    zero_prob = zero_proba.to_json(orient='records')
+    return zero_prob
 
 #lancement de l'application
 if __name__ == "__main__":
